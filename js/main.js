ea9a02e86d4aa9b62000c4f4528ba98e65b91a4e
cowgodmoo <131647948+cowgodmoo@users.noreply.github.com>
2023-05-10 15:37:48 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,81 @@
+$(function() {
+
+  'use strict';
+
+  $('.js-menu-toggle').click(function(e) {
+
+  	var $this = $(this);
+
+  	
+
+  	if ( $('body').hasClass('show-sidebar') ) {
+  		$('body').removeClass('show-sidebar');
+  		$this.removeClass('active');
+  	} else {
+  		$('body').addClass('show-sidebar');	
+  		$this.addClass('active');
+  	}
+
+  	e.preventDefault();
+
+  });
+
+  // click outisde offcanvas
+	$(document).mouseup(function(e) {
+    var container = $(".sidebar");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ( $('body').hasClass('show-sidebar') ) {
+				$('body').removeClass('show-sidebar');
+				$('body').find('.js-menu-toggle').removeClass('active');
+			}
+    }
+	}); 
+
+    
+
+});
+
+/* filter */
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
